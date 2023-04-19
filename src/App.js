d54d33756701276744e1aa3ec3ce0203cf83893f
Guilherme Libanori <guilhermelibanori1@gmail.com>
2023-04-19 17:10:02 -0300

    Fix Eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-
 import { Route, Switch } from 'react-router-dom';
 import { Login } from './pages/Login';
-import { Game } from './pages/Game';
+import Game from './pages/Game';
 
 class App extends React.Component {
   render() {
