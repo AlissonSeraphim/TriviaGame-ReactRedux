17e4772f5afa6cda1bf9d6a8bc5ae901932131a2
Amanda Rodrigues <rodriguesamanda49@gmail.com>
2023-04-20 19:39:17 -0300

    Requisito 12: dependendo de requisitos anteriores

---
+++ src/pages/Feedback.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export class Feedback extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
