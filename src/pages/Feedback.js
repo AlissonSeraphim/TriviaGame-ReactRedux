8c6ed3271cc106d21219e2e49f7fb4a0f8c3db97
AlissonSeraphim <projetoseraphim@gmail.com>
2023-04-24 18:53:14 -0300

    Requisito 11 Concluido, cheat: score infinito

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,7 +5,7 @@ export class Feedback extends React.Component {
   render() {
     return (
       <div>
-        <h1>feedback page</h1>
+        <h1 data-testid="feedback-text">feedback page</h1>
         <Header />
       </div>
     );
