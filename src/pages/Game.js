11e425f324fce65b89328de51a49146baaad4fc7
Amanda Rodrigues <rodriguesamanda49@gmail.com>
2023-04-24 16:39:26 -0300

    requisito 15: funcional porém não passando nos testes, dependente do contador

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -115,7 +115,7 @@ class Game extends React.Component {
       timer,
     } = this.state;
 
-    // const { history } = this.props;
+    const { history } = this.props;
 
     return (
       <div>
@@ -168,6 +168,12 @@ class Game extends React.Component {
               Next Button
             </button>
           )}
+          <button
+            data-testid="btn-play-again"
+            onClick={ () => history.push('/') }
+          >
+            Play Again
+          </button>
         </div>
       </div>
     );
